f8fce3ccc2313f71f8f676d9d2c3e5e9742e0981
Artur Shaidullin <ururu2909@gmail.com>
2024-07-30 12:15:49 +0400

    build timestamps database

--- timestamps-database/src/main/java/ru/arutr/timestampsdatabase/TimeStampsDatabase.kt
+++ timestamps-database/src/main/java/ru/arutr/timestampsdatabase/TimeStampsDatabase.kt
@@ -1,4 +1,21 @@
 package ru.arutr.timestampsdatabase
 
-class TimeStampsDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import ru.arutr.timestampsdatabase.dao.TimeStampDao
+import ru.arutr.timestampsdatabase.models.TimeStampDBO
+
+@Database(entities = [TimeStampDBO::class], version = 1)
+abstract class TimeStampsDatabase : RoomDatabase() {
+    abstract fun timeStampsDao(): TimeStampDao
+
 }
+
+fun TimeStampsDatabase(applicationContext: Context): TimeStampsDatabase =
+    Room.databaseBuilder(
+        context = applicationContext.applicationContext,
+        klass = TimeStampsDatabase::class.java,
+        name = "timestamps-database"
+    ).build()
