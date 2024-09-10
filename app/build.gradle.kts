5525db7be107c192663b321420f4053734317052
Artur Shaidullin <ururu2909@gmail.com>
2024-09-10 11:20:08 +0400

    add common module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,6 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     debugImplementation(libs.androidx.ui.tooling)
+
+    implementation(project(":common"))
 }
