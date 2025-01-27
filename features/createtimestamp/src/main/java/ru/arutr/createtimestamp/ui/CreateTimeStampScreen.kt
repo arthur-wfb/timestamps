25bc749711e4686cc18312df7cd2cef7f687b901
Artur Shaidullin <ururu2909@gmail.com>
2025-01-27 23:06:42 +0400

    add date picker on create screen

--- features/createtimestamp/src/main/java/ru/arutr/createtimestamp/ui/CreateTimeStampScreen.kt
+++ features/createtimestamp/src/main/java/ru/arutr/createtimestamp/ui/CreateTimeStampScreen.kt
@@ -6,9 +6,13 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,8 +23,15 @@ fun CreateTimeStampScreen() {
     Content()
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun Content() {
+    val datePickerState = rememberDatePickerState()
+
+    LaunchedEffect(datePickerState.selectedDateMillis) {
+        // TODO: onDateChanged
+    }
+
     Column(
         modifier = Modifier
             .fillMaxHeight()
@@ -41,6 +52,10 @@ private fun Content() {
             value = "",
             onValueChange = {},
             label = { Text(text = "Enter description") },
+        )
+        DatePicker(
+            modifier = Modifier.padding(top = 16.dp),
+            state = datePickerState,
         )
         Button(
             modifier = Modifier.fillMaxWidth(),
